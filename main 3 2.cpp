832b919bc5883630194e050ac5a9389ecf5ddbfd
Komrade FMX <62594965+FMX372@users.noreply.github.com>
2022-04-09 12:13:30 +0200

    Update main 3 2.cpp

--- main 3 2.cpp
+++ main 3 2.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-    string name,respuesta;
+    string name,respuesta,accion,dar;
     cout<<"Estás haciendo un viaje en Noruega, 1532, un viajero se te acerca."<<endl;
     cout<<"Viajero: Buenas viajero, ¿puedo preguntarle su nombre?"<<endl<<"Nombre: ";
     cin>>name;
@@ -16,18 +16,47 @@ int main()
     }
     else
     {
-    cout<<"Viajero: Encantado de conocerle "<<name<<", me preguntaba si podrías hacerme un favor, te pagaré bien."<<endl<<name<<":";
+    cout<<"Viajero: Encantado de conocerle "<<name<<", me preguntaba si podrías hacerme un favor, te pagaré bien. "<<endl<<name<<": ";
     }
     cin>>respuesta;
         if (respuesta == "Si")
         {
-            cout<<"Viajero: Llevo 1 semana sin comer tomate, si no le es mucha molestia, vaya a la posada más cercana y compreme un tomate, le esperaré aquí"<<endl;
+            cout<<"Viajero: Llevo 1 semana sin comer tomate, si no le es mucha molestia, vaya a la posada más cercana y compreme un tomate, le esperaré aquí."<<endl;
         }
         else
         {
             cout<<"Viajero: Siga su camino entonces";
             return 0;
         }
-    cout<<"Misión: Dar un tomate al viajero"<<endl<<"Opciones:"<<endl<<"1.Ir cuesta abajo"<<endl<<"2.Preguntar para qué quiere un tomate"<<endl<<"3.Buscar en tu zurrón";
+    cout<<"Misión: Dar un tomate al viajero"<<endl<<"Opciones:"<<endl<<"1.Ir cuesta abajo"<<endl<<"2.Preguntar para qué quiere un tomate"<<endl<<"3.Buscar en tu zurrón"<<endl<<"Opción: ";
+    cin>>accion;
+    /*Opciones a elegir*/
+        if (accion == "1")
+    {
+            cout<<endl<<"Bajas la cuesta y llegas a una taberna";
+    }
+        if (accion == "2")
+    {
+            cout<<"Viajero: Es una larga historia, prometí comer tomate todos los meses a alguien.";
+            
+    }
+        if (accion == "3")
+    {
+            cout<<"Encuentras un tomate en un estado cuestionable."<<endl;
+            cout<<name<<"¿Se lo doy?: ";
+            cin>>dar;
+                if (dar == "Si")
+                {
+                    cout<<"Viajero: Esto me servirá, te estaré eternamente agradecido, tome, para usted."<<endl<<"Recibes 15 monedas de plata.";
+                    return 0;
+                }
+            else
+            {
+                cout<<name<<": tendré que buscar en otra parte.";
+            }
+            
+            
+    }
     return 0;
 }
+    
