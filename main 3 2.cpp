f8966c272a8a6fd8eee0db6c5c9ae03fd1f2cd73
Komrade FMX <62594965+FMX372@users.noreply.github.com>
2022-04-14 11:24:55 +0200

    Update main 3 2.cpp

--- main 3 2.cpp
+++ main 3 2.cpp
@@ -21,7 +21,7 @@ int main()
     cin>>respuesta;
         if (respuesta == "Si")
         {
-            cout<<"Viajero: Llevo 1 semana sin comer tomate, si no le es mucha molestia, vaya a la posada más cercana y compreme un tomate, le esperaré aquí."<<endl;
+            cout<<"Viajero: Llevo 1 semana sin comer tomate, si no le es mucha molestia, vaya a la posada más cercana y compreme un tomate, le esperaré aquí."<<endl<<endl;
         }
         else
         {
@@ -37,8 +37,15 @@ int main()
     }
         if (accion == "2")
     {
-            cout<<"Viajero: Es una larga historia, prometí comer tomate todos los meses a alguien.";
-            cout<<name<<": Un poco raro";
+            cout<<endl<<"Viajero: Es una larga historia, prometí comer tomate todos los meses a alguien."<<endl;
+            cout<<name<<": Un poco raro..."<<endl<<endl;
+            cout<<"Opciones:"<<endl<<"1.Ir cuesta abajo."<<endl<<"2.Buscar en tu zurrón."<<endl<<"Opción: ";
+            cin>>accion;
+        /*Sub-acciones de acción 2*/
+            if (accion == "1")
+            {
+                cout<<endl<<"Bajas la cuesta y llegas a una taberna";
+            }
             
     }
         if (accion == "3")
@@ -54,6 +61,21 @@ int main()
             else
             {
                 cout<<name<<": tendré que buscar en otra parte.";
+                cout<<"Opciones:"<<endl<<"1.Ir cuesta abajo."<<endl<<"2.Preguntar para qué quiere un tomate."<<endl<<"Opción: ";
+                cin>>accion;
+            /*Sub-acciones de acción 3*/
+                if (accion == "1")
+                {
+                    cout<<endl<<"Bajas la cuesta y llegas a una taberna";
+                }
+                if (accion == "2")
+                {
+                    cout<<"Viajero: Es una larga historia, prometí comer tomate todos los meses a alguien.";
+                    cout<<name<<": Un poco raro";
+            
+            
+                }
+                
             }
             
             
